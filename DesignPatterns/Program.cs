0103192d3c908c59ff99ea6d41f2f5aca327b67d
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-08 17:11:05 -0300

    first commit

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DesignPatterns.SingletonPattern;
+
+namespace DesignPatterns
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Singleton Pattern");
+    }
+  }
+}
